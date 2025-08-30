6ed17fbeabbdc042a755a3925f948d7c88b414d1
GeraldoA07 <geraldoartemius2005@gmail.com>
2025-08-30 16:39:38 +0700

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+function Calculator() {
+  return <div className="bg-white"></div>;
+}
+
+function App() {
+  return (
+    <div className="bg-linear-to-br from-slate-800 to-slate-950  h-dvh w-full flex items-center justify-center relative overflow-clip">
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
