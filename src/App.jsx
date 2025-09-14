1285f17d86891c4565c9bc087afb2832893b6218
GeraldoA07 <geraldoartemius2005@gmail.com>
2025-09-14 17:30:11 +0700

    Add design to background

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function Calculator() {
 
 function App() {
   return (
-    <div className="bg-linear-to-br from-slate-800 to-slate-950  h-dvh w-full flex items-center justify-center relative overflow-clip">
+    <div className="h-dvh w-full flex items-center justify-center relative overflow-clip bg-cover bg-center bg-no-repeat bg-[url('https://wallpapers.com/images/hd/aesthetic-gray-3180-x-1960-background-j0wjmhe89xylw8qj.jpg')]">
       <Calculator />
     </div>
   );
